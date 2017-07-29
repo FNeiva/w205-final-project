b169bded970724f5ca1ca6120a0e19bf3d9b2d1d
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2017-07-29 17:36:11 -0300

    Minor cosmetic changes to data lake load

--- data_layer/load_data_lake.sh
+++ data_layer/load_data_lake.sh
@@ -24,50 +24,50 @@ echo "Loading Data Lake:"
 TMP_DIR=/data/tmp_data
 
 # Remove old temporary directory if exists and then recreate
-echo "		Creating temporary directory..."
+echo "	* Creating temporary directory..."
 if [ -d $TMP_DIR ] ; then
 	rm -r $TMP_DIR
 fi
 mkdir $TMP_DIR
-echo "		Temporary directory created!"
+echo "	* Temporary directory created!"
 
 
 # Download files to the temporary directory
-echo "		Downloading data files to temporary directory..."
+echo "	* Downloading data files to temporary directory..."
 wget -O $TMP_DIR/dengai_train_feature_data.csv "https://s3.amazonaws.com/drivendata/data/44/public/dengue_features_train.csv"
 wget -O $TMP_DIR/dengai_train_labels_data.csv "https://s3.amazonaws.com/drivendata/data/44/public/dengue_labels_train.csv"
 wget -O $TMP_DIR/dengai_test_feature_data.csv "https://s3.amazonaws.com/drivendata/data/44/public/dengue_features_test.csv"
-echo "		Data downloaded successfully!"
+echo "	* Data downloaded successfully!"
 
 # Strip the first line and rename the files we are interested in
-echo "		Stripping data headers..."
+echo "	* Stripping data headers..."
 tail -n +2 $TMP_DIR/"dengai_train_feature_data.csv" > $TMP_DIR/dengai_train_feature_noheader.csv
 tail -n +2 $TMP_DIR/"dengai_train_labels_data.csv" > $TMP_DIR/dengai_train_labels_noheader.csv
 tail -n +2 $TMP_DIR/"dengai_test_feature_data.csv" > $TMP_DIR/dengai_test_feature_noheader.csv
-echo "		Data headers stripped!"
+echo "	* Data headers stripped!"
 
 # Clean existing directories and files in HDFS to remove possible old files
-echo "		Clear existing data lake directory..."
+echo "	* Clear existing data lake directory..."
 hdfs dfs -rm -r "/user/w205/dengue_prediction/original_data"
-echo "		Data lake directory cleared!"
+echo "	* Data lake directory cleared!"
 
 # Recreate directory structure in the HDFS data lake
-echo "		Creating data lake directory structure..."
+echo "	* Creating data lake directory structure..."
 hdfs dfs -mkdir "/user/w205/dengue_prediction"
 hdfs dfs -mkdir "/user/w205/dengue_prediction/original_data"
-echo "		Directory structure created!"
+echo "	* Directory structure created!"
 
 # Load files into the HDFS data lake directory structure
-echo "		Loading files into the data lake..."
+echo "	* Loading files into the data lake..."
 hdfs dfs -put $TMP_DIR/dengai_train_feature_noheader.csv "/user/w205/dengue_prediction/original_data"
 hdfs dfs -put $TMP_DIR/dengai_train_labels_noheader.csv "/user/w205/dengue_prediction/original_data"
 hdfs dfs -put $TMP_DIR/dengai_test_feature_noheader.csv "/user/w205/dengue_prediction/original_data"
-echo "		Data files loaded into the data lake!"
+echo "	* Data files loaded into the data lake!"
 
 # Remove temporary data directory used for extracting the raw data files
-echo "		Removing temporary directory..."
+echo "	* Removing temporary directory..."
 rm -r $TMP_DIR
-echo "		Temporary directory removed!"
+echo "	* Temporary directory removed!"
 
 # All done
 echo "Data lake loading finished successfully!"
