fa529135eaa23ead6f406c7a6c70b69e80a9c4fe
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2017-08-15 15:37:22 -0300

    Created directory structure and data layer master script

---
+++ setup_data_layer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+##########################################################################################
+# Dengue Fever Prediction System
+# W205 Summer 2017 Final Project
+# Felipe Campos, Frank Shannon, Josh Wilson and Matthew Holmes
+##########################################################################################
+# Data Layer Initialization Script
+#
+# This is the super script that runs the entire Data Layer pipeline. It will:
+#
+#     * Download the required data
+#     * Work the downloaded data and put it in the appropriate format
+#     * Create the HDFS file structure and load the original data into HDFS
+#     * Transform the data to be used by the other layers
+#     * Store the transformed in HDFS
+#     * Create the Hive/SparkSQL schema to access the data
+#
+##########################################################################################
+
+echo "################################################"
+echo "Dengue Fever Prediction System"
+echo "Data Layer Initialization Script"
+echo "################################################"
+echo " "
+echo "This script will now setup and initialize the Data Layer."
+echo "It will run other scripts to download, transform and store the required data."
+echo "This will probably take a few minutes, so please wait until it is finished."
+echo " "
+echo " "
+echo "Step 1 of 3: Running Data Layer Loading Script... "
+echo " "
+echo " "
+./data_layer/load_data_lake.sh
+echo " "
+echo "Step 1 of 3 done."
+echo " "
+echo "Step 2 of 3: Running Data Layer Transformation Script... "
+echo " "
+echo " "
+/data/spark15/bin/spark-submit --packages com.databricks:spark-csv_2.10:1.5.0 ./data_layer/transform_data.py
+echo " "
+echo "Step 2 of 3 done."
+echo " "
+echo "Step 3 of 3: Creating Hive/SparkSQL schema... "
+echo " "
+echo " "
+/data/spark15/bin/spark-sql -f ./data_layer/create_schema.sql
+echo " "
+echo "Step 3 of 3 done."
+echo " "
+echo "Data Layer Initialization Script Finished!"
