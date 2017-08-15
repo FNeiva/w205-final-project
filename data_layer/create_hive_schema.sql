7847eceb1cb3610680b39b52db12929660ca2a02
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2017-08-15 00:23:43 -0300

    Modified Hive script

--- data_layer/create_hive_schema.sql
+++ data_layer/create_hive_schema.sql
@@ -12,23 +12,67 @@
 
 -- Drop possible pre-existing old tables before recreating
 
-DROP TABLE dengue_history_data;
+DROP TABLE dengai_original_data;
 
--- Create Dengue History table
+-- Create DengAI original data table
 --
--- This table stores the historical dengue data
+-- This table stores the original DengAI data
 
-CREATE EXTERNAL TABLE dengue_history_data (
+CREATE EXTERNAL TABLE dengai_original_data (
   city string,
   year string,
   week_of_year string,
+  week_start_date string,
+  veg_ne string,
+  veg_nw string,
+  veg_se string,
+  veg_sw string,
+  precip_mm string,
+  air_temp_K string,
   avg_temp_K string,
   dew_point_temp string,
   max_temp_K string,
   min_temp_K string,
+  precip_kg_m2 string,
   rel_hum_pct string,
+  sat_precip_mm string,
+  spec_hum_g_kg string,
+  tdtr_K string,
   avg_temp_C string,
+  station_diurn_temp_C string,
+  station_max_temp_C string,
+  station_min_temp_C string,
+  station_precip_mm string,
+  city_rep string,
+  year_rep string,
+  week_of_year_rep string,
   num_cases string
 )
+ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde'
+WITH SERDEPROPERTIES (
+  "separatorChar" = ",",
+  "quoteChar" = '"',
+  "escapeChar" = '\\'
+)
 STORED AS TEXTFILE
-LOCATION '/user/w205/dengue_prediction/transformed_data/dengue_data';
+LOCATION '/user/w205/dengue_prediction/original_data/dengai_data.csv';
+
+
+-- Create Dengue History data table
+--
+-- This table stores the transformed Dengue History data
+
+CREATE TABLE dengue_history_data AS
+    SELECT
+      city,
+      year,
+      week_of_year,
+      CAST(avg_temp_K AS DOUBLE) AS avg_temp_K,
+      CAST(dew_point_temp AS DOUBLE) AS dew_point_temp,
+      CAST(max_temp_K AS DOUBLE) AS max_temp_K,
+      CAST(min_temp_K AS DOUBLE) AS min_temp_K,
+      CAST(rel_hum_pct AS DOUBLE) AS rel_hum_pct,
+      CAST(avg_temp_C AS DOUBLE) AS avg_temp_C,
+      CAST(num_cases AS INT) AS num_cases
+    FROM
+      dengai_transf_data;
