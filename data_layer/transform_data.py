f42a24369b8c49f7f81d04a2b709c2472bacba9d
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2017-08-15 22:38:09 -0300

    Minor fix to transform script

--- data_layer/transform_data.py
+++ data_layer/transform_data.py
@@ -171,11 +171,9 @@ datasus_notif_df = datasus_notif_df.groupBy(["city","year","wkofyear"]).count()
 # Rename count column
 datasus_notif_df = datasus_notif_df.selectExpr("city","year","wkofyear","count as num_cases")
 # Join dataframes
-print(datasus_notif_df.columns)
-print(datasus_weather_df.columns)
-join_condition = [datasus_notif_df.city == datasus_weather_df.city,
-                  datasus_notif_df.year == datasus_weather_df.year,
-                  datasus_notif_data.wkofyear == datasus_weather_df.wkofyear]
+join_condition = [datasus_notif_df["city"] == datasus_weather_df["city"],
+                  datasus_notif_df["year"] == datasus_weather_df["year"],
+                  datasus_notif_data["wkofyear"] == datasus_weather_df["wkofyear"]]
 datasus_df = datasus_notif_df.join(datasus_weather_df,join_condition,"inner")
 # Change city geocode by city name
 # Ideally, this would be done by using the cities data, but for now we'll use a simple dictionary
