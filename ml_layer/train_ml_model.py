96231432a5143e0d5e4471b1bd4a07701a90500b
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2017-08-17 17:14:12 -0300

    Fixed ML training by using RDDs only

--- ml_layer/train_ml_model.py
+++ ml_layer/train_ml_model.py
@@ -12,13 +12,14 @@
 #
 ##########################################################################################
 
+import numpy as np
 import pyspark
 import pyspark.sql.functions as psf
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.regression import LinearRegressionWithSGD
+from pyspark.mllib.tree import GradientBoostedTrees
 from pyspark.mllib.linalg import SparseVector
 from pyspark.mllib.evaluation import RegressionMetrics
 
@@ -40,68 +41,30 @@ training_data = sc.textFile("hdfs:///user/w205/dengue_prediction/transformed_dat
 # Split the data on ',''
 training_data = training_data.map(lambda x: x.split(','))
 # Map columns
-training_data = training_data.map(lambda x: (int(x[0]),int(x[1]),int(x[2]),int(x[3]),float(x[4]),float(x[5]),float(x[6]),float(x[7]),float(x[8]),float(x[9]),int(x[10])))
-
-# Build the schema and construct the Data Frame
-schemaString = 'city0 city1 city2 city3 avg_temp_K dew_pt_temp_K max_temp_K min_temp_K rel_hum_pct avg_temp_C num_cases'
-fields = [StructField(field_name, StringType(), True) for field_name in schemaString.split()]
-schema = StructType(fields)
-training_df = sqlContext.createDataFrame(training_data, schema).cache()
-
-# Clear rows with missing values, for sanity checking
-for col in training_df.columns:
-    training_df = training_df.filter(training_df[col].isNotNull())
+training_data = training_data.map(lambda x: (int(x[0]),int(x[1]),int(x[2]),int(x[3]),
+                                             int(x[4]),float(x[5]),float(x[6]),float(x[7]),
+                                             float(x[8]),float(x[9]),float(x[10]),int(x[11])))
+# Filter NaNs that may have made it into the dataset
+training_data = training_data.filter(lambda x: ~np.isnan(x[0]) and ~np.isnan(x[1]) and ~np.isnan(x[2]) and ~np.isnan(x[3]) and
+                                               ~np.isnan(x[4]) and ~np.isnan(x[5]) and ~np.isnan(x[6]) and ~np.isnan(x[7]) and
+                                               ~np.isnan(x[8]) and ~np.isnan(x[9]) and ~np.isnan(x[10]) and ~np.isnan(x[11])).cache()
 
 print("     * Transformed data read!")
-#print("     * Reshaping the data for ML training... ")
-
-# We need to make dummy variables for each city
-#cities = training_df.select("city").distinct().rdd.map(lambda r: r[0]).collect()
-#colnames = []
-#for city in cities:
-#    column_name = "city_"+city.replace(" ","_")
-#    training_df = training_df.withColumn(column_name, psf.when(training_df["city"] == city,1).otherwise(0))
-#    colnames.append(column_name)
-# Drop one city from dummy variable to avoid multicollinearity
-#colnames = colnames[:-1]
-
-# Change column types
-#training_df = training_df.withColumn("avg_temp_K", training_df["avg_temp_K"].cast(DoubleType()))
-#training_df = training_df.withColumn("dew_pt_temp_K", training_df["dew_pt_temp_K"].cast(DoubleType()))
-#training_df = training_df.withColumn("max_temp_K", training_df["max_temp_K"].cast(DoubleType()))
-#training_df = training_df.withColumn("min_temp_K", training_df["min_temp_K"].cast(DoubleType()))
-#training_df = training_df.withColumn("rel_hum_pct", training_df["rel_hum_pct"].cast(DoubleType()))
-#training_df = training_df.withColumn("avg_temp_C", training_df["avg_temp_C"].cast(DoubleType()))
-#training_df = training_df.withColumn("num_cases", training_df["num_cases"].cast(IntegerType()))
-
-
-
-# Select columns to be used
-#colnames.append("avg_temp_K")
-#colnames.append("dew_pt_temp_K")
-#colnames.append("max_temp_K")
-#colnames.append("min_temp_K")
-#colnames.append("rel_hum_pct")
-#colnames.append("avg_temp_C")
-#colnames.append("num_cases")
-#training_df = training_df.select(colnames).cache()
-
-#print("     * Data reshape finished!")
 print("     * Training test ML model... ")
 
 # Label the data points
-labeled_data = training_df.map(lambda x: LabeledPoint(x[-1],x[:-1])).cache()
+labeled_data = training_data.map(lambda x: LabeledPoint(x[-1],x[:-1]))
 # Separate training and testing data
-training_data, testing_data = labeled_data.randomSplit([0.8, 0.2])
+train_data, test_data = labeled_data.randomSplit([0.8, 0.2])
 # Train the model
-ml_model = LinearRegressionWithSGD.train(training_data, iterations=100)
+ml_model = GradientBoostedTrees.trainRegressor(train_data, {}, numIterations = 20, loss='leastAbsoluteError')
 
 print("     * Model trained!")
 print("     * Testing model error... ")
 
 # Predict and calculate error metrics
-predictions = ml_model.predict(testing_data.map(lambda r: r.features))
-predictions.zip(testing_data.map(lambda r: r.label))
+predictions = ml_model.predict(test_data.map(lambda r: r.features))
+predictions.zip(test_data.map(lambda r: r.label))
 metrics = RegressionMetrics(predictions)
 
 print("     * Model regression error metrics: ")
@@ -111,7 +74,7 @@ print("         - Root Mean Squared Error: %.2f" % metrics.rootMeanSquaredError)
 print("     * Training model with full data... ")
 
 # Re-train model using full dataset
-ml_model = LinearRegressionWithSGD.train(labeled_data, iterations=100)
+ml_model = GradientBoostedTrees.trainRegressor(labeled_data, {}, numIterations = 20, loss='leastAbsoluteError')
 
 print("     * Persisting model to HDFS... ")
 
